608b998c7f6beff5fb13f02ea02076bfa1f7ad6b
Manuel <alonsodelacruz109@gmail.com>
2025-11-17 00:30:29 -0500

    Actualizar imágenes a ImgBB

--- porjects/00-hola-mundo/src/componentes/hero_logo.tsx
+++ porjects/00-hola-mundo/src/componentes/hero_logo.tsx
@@ -2,7 +2,7 @@
 export const Hero_logo = () => {
   return (
     <section className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
-        <img src="/images/logo_texto_icon.png" alt="" className="w-[520px]" />
+        <img src="https://ibb.co/V0MKSXb1" alt="" className="w-[520px]" />
     </section>
   )
 }
--- porjects/00-hola-mundo/src/componentes/hero_obras.tsx
+++ porjects/00-hola-mundo/src/componentes/hero_obras.tsx
@@ -2,16 +2,16 @@ import { ImagenFade } from "./ImagenFade"
 export const Hero_obras = () => {
   return (
         <section className="flex z-10">
-            <ImagenFade src="/images/pieza_1.png" alt="pieza 1" />
-            <ImagenFade src="/images/pieza_2.png" alt="pieza 2" />
-            <ImagenFade src="/images/pieza_3.png" alt="pieza 3" />
-            <ImagenFade src="/images/pieza_4.png" alt="pieza 4" />
-            <ImagenFade src="/images/pieza_5.png" alt="pieza 5" />
-            <ImagenFade src="/images/pieza_6.png" alt="pieza 6" />
-            <ImagenFade src="/images/pieza_7.png" alt="pieza 7" />
-            <ImagenFade src="/images/pieza_8.png" alt="pieza 8" />
-            <ImagenFade src="/images/pieza_9.png" alt="pieza 9" />
-            <ImagenFade src="/images/pieza_10.png" alt="pieza 10" />
+            <ImagenFade src="https://ibb.co/CKwvztKH" alt="pieza 1" />
+            <ImagenFade src="https://ibb.co/6cVm6fHF" alt="pieza 2" />
+            <ImagenFade src="https://ibb.co/Y4sWs6F0" alt="pieza 3" />
+            <ImagenFade src="https://ibb.co/dJjBG0KZ" alt="pieza 4" />
+            <ImagenFade src="https://ibb.co/b5W4rqKP" alt="pieza 5" />
+            <ImagenFade src="https://ibb.co/fw3NFdp" alt="pieza 6" />
+            <ImagenFade src="https://ibb.co/Qvg2b1Yr" alt="pieza 7" />
+            <ImagenFade src="https://ibb.co/fzYTc1Kc" alt="pieza 8" />
+            <ImagenFade src="https://ibb.co/gFQTsyy2" alt="pieza 9" />
+            <ImagenFade src="https://ibb.co/x81L86NJ" alt="pieza 10" />
         </section>
   )
 }
